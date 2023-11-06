b146f1afe8ed49bddb44afcc8858302c6bb0c035
Romit Dasgupta <98577369+romitdasgupta@users.noreply.github.com>
2023-11-05 19:40:24 -0800

    Exercise 1

---
+++ src/components/FormInput/FormInput.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function FormInput() {
+  const [guess, setGuess] = React.useState("");
+  function handleGuess(event) {
+    const lastChar = event.target.value.toUpperCase()[event.target.value.length - 1];
+    if (lastChar < 'A' || lastChar > 'Z') return;
+
+    if (event.target.value.length === 6) {
+      return;
+    }
+    setGuess(event.target.value.toUpperCase());
+  }
+
+  function handleFormSubmission(event) {
+    event.preventDefault();
+    if (guess.length !== 5) {
+      return;
+    }
+    console.log('form submitted with value:', guess);
+    setGuess("");
+  }
+  return (
+    <div>
+      <form className="guess-input-wrapper" onSubmit={handleFormSubmission}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input required minLength={5} maxLength={5} pattern="[a-zA-Z]{5}" id="guess-input" type="text"  onChange={handleGuess} value={guess}/>
+      </form>
+    </div>
+  );
+}
+
+export default FormInput;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,19 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
-
+import FormInput from '../FormInput';
 // Pick a random word on every pageload.
+
 const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  return (
+  <>
+    Put a game here!
+    <FormInput/>
+  </>);
 }
 
 export default Game;
